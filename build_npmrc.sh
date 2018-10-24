6a8423d155905e579ec2d41f53ab7cea9fa0a81b
Steve Rutherford <steve.rutherford.contractor@macmillan.com>
2018-10-24 15:21:24 -0500

    Use full npmrc

--- build_npmrc.sh
+++ build_npmrc.sh
@@ -2,4 +2,5 @@
 
 curl -u $NPM_USER:$NPM_PASSWORD https://registry.sh.mml.cloud/artifactory/api/npm/auth > .npmrc
 curl -u $NPM_USER:$NPM_PASSWORD https://registry.sh.mml.cloud/artifactory/api/npm/npm-local/auth/macmillan-learning >> .npmrc
+echo 'registry=https://registry.sh.mml.cloud/artifactory/api/npm/npm/' >> .npmrc
 cat .npmrc
