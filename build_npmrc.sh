a14ea0ee1b09787619a63539f5d8b801e5d8e32e
Steve Rutherford <steve.rutherford.contractor@macmillan.com>
2018-10-31 16:50:59 -0500

    Check for variables before curl

--- build_npmrc.sh
+++ build_npmrc.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [[ -z $NPM_USER ]]; then
+  echo 'NPM_USER is not set'
+  exit 1
+elif [[ -z $NPM_PASSWORD ]]; then
+  echo 'NPM_PASSWORD is not set'
+  exit 2
+fi
+
 curl -u $NPM_USER:$NPM_PASSWORD https://registry.sh.mml.cloud/artifactory/api/npm/auth > .npmrc
 curl -u $NPM_USER:$NPM_PASSWORD https://registry.sh.mml.cloud/artifactory/api/npm/npm-local/auth/macmillan-learning >> .npmrc
 echo 'registry=https://registry.sh.mml.cloud/artifactory/api/npm/npm/' >> .npmrc
