10f7693a826aa078baca802b0248638f3336c695
Steve Rutherford <steve.rutherford.contractor@macmillan.com>
2018-12-28 15:09:09 -0800

    Update registry

--- build_npmrc.sh
+++ build_npmrc.sh
@@ -10,5 +10,5 @@ fi
 
 curl -u $NPM_USER:$NPM_PASSWORD https://registry.sh.mml.cloud/artifactory/api/npm/auth > .npmrc
 curl -u $NPM_USER:$NPM_PASSWORD https://registry.sh.mml.cloud/artifactory/api/npm/npm-local/auth/macmillan-learning >> .npmrc
-echo 'registry=https://registry.sh.mml.cloud/artifactory/api/npm/npm/' >> .npmrc
+echo registry=https://registry.npmjs.org >> .npmrc
 cat .npmrc
